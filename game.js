bbaf0d045f4eff1ea606d5645ec67b825b971a62
Carmen Zhang <jwzhang06@gmail.com>
2022-11-03 09:38:31 -0400

    hi

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
-function startGame(){
-    const intervalID = setInterval(startGame, 30000);
-    setTimeout(() => { // do something 
-        clearInterval(intervalID);
-    }, 10 * 1000);
+const intervalID = setInterval(timeCount, 1000);
+const timeShow = document.getElementById("timer");
+let time = 31;
+
+function timeCount(){
+
+    time -= 1;
+    timeShow.innerText = time;
+    
 }
+
+setTimeout(() => { 
+    clearInterval(intervalID);
+}, (30 * 1000));
