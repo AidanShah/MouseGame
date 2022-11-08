bf7e95b34fbf4853aed539f34bddf001a1ad0b8a
Aidan Shah <aidanshah@Aidans-MacBook-Pro.local>
2022-11-08 09:35:03 -0500

    target

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let intervalID = 0;
 const TIME_REMAINING = 5;
 let time = TIME_REMAINING;
+let score = 0;
 function startGame(){
     if(intervalID != 0){
         return;
@@ -16,7 +17,12 @@ function startGame(){
         timeShow.innerText = "Game Over";
 
     }, (TIME_REMAINING * 1000));
-
+    var myDiv = document.getElementById("GFG");
+    var button = document.createElement('BUTTON');
+    button.setAttribute("id","target");
+    button.setAttribute("class","target");
+    button.setAttribute("onclick","pressButton()");
+    myDiv.appendChild(button);
     randomPos();
 }
 
@@ -27,15 +33,24 @@ function timeCount(){
     timeShow.innerText = "Time Remaining: " + time;
 }
 
-target.onclick = function pressButton(){
+function pressButton(){
     score += 1;
     document.getElementById("score").innerText = " Score: " + String(score) + " ";
+    let die = getElementById("target");
+    die.remove(); 
+    var myDiv = document.getElementById("GFG");
+    var button = document.createElement('BUTTON');
+    button.setAttribute("id","target");
+    button.setAttribute("class","target");
+    button.setAttribute("onclick","pressButton()");
+    myDiv.appendChild(button);
+    randomPos();
 };
 
 
 
 function randomPos(){
-    const target = document.getElementById("redButton");
+    const target = document.getElementById("target");
     let height = document.body.clientHeight;
     let width = document.body.clientWidth;
     let randHeight = Math.floor(Math.random() * (height - target.offsetHeight));
