8e2ef94ddaa5bd2c87d84e927b7fc964800efa86
Aidan Shah <aidanshah@Aidans-MacBook-Pro.local>
2022-11-08 09:25:55 -0500

    m

--- Mousegame.js
+++ Mousegame.js
@@ -1,6 +1,12 @@
 function startGame(){
     const intervalID = setInterval(timeCount, 1000);
     timeShow.innerText = "Time Remaining: " + time;
+    var myDiv = document.getElementById("GFG");
+    var button = document.createElement('BUTTON');
+    button.setAttribute("id","target");
+    button.setAttribute("class","target");
+    button.setAttribute("onclick","pressButton()");
+    myDiv.appendChild(button);
 }
 
 const timeShow = document.getElementById("timer");
@@ -8,12 +14,21 @@ let time = 30;
 let score = 0;
 function timeCount(){
     time -= 1;
+    timeShow.innerText = "Time Remaining: " + time;
 }
 
 setTimeout(() => { 
     clearInterval(intervalID);
 }, (31 * 1000));
-target.onclick = function pressButton(){
+function pressButton(){
     score += 1;
     document.getElementById("score").innerText = " Score: " + String(score) + " ";
+    let die = getElementById("target");
+    die.remove(); 
+    var myDiv = document.getElementById("GFG");
+    var button = document.createElement('BUTTON');
+    button.setAttribute("id","target");
+    button.setAttribute("class","target");
+    button.setAttribute("onclick","pressButton()");
+    myDiv.appendChild(button);
 };
