d9debec146e5e42eb9ab7c6dc1714579060afb5f
Aidan Shah <aidanshah@Aidans-MacBook-Pro.local>
2022-11-10 09:39:57 -0500

    deleted mouse game

--- gametest..js
+++
@@ -1,9 +0,0 @@
-
-
-function randomPos(){
-    let height = document.body.clientHeight
-    let width = document.body.clientWidth
-    let randHeight = Math.floor(Math.random() * height)
-    let randWidth = Math.floor(Math.random() * width)
-    return [randHeight, randWidth]
-}
